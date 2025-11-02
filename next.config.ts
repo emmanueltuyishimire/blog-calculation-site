3a7a269ec6406d0f4d4831bd96874631172fc644
lofi boy <lofiboyog@gmail.com>
2025-11-02 20:55:05 +0000

    What did you do? the change you made caused 404 This page not found erro

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
 import type {NextConfig} from 'next';
 
-const repo = 'your-repo-name'; // IMPORTANT! Change this to your repository name.
-const assetPrefix = `/${repo}/`;
-const basePath = `/${repo}`;
-
 const nextConfig: NextConfig = {
-  output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    unoptimized: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
