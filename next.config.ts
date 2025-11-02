156cc682a827f0651e9ad17a8f835682e8a5d754
lofi boy <lofiboyog@gmail.com>
2025-11-02 21:06:17 +0000

    my ripo name is blog-calculation-site

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,21 @@
 import type {NextConfig} from 'next';
 
+const repo = 'blog-calculation-site';
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
+
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    unoptimized: true,
   },
 };
 
