4a495357988617a42f43a342ffdc6e73cca9c6bc
lofi boy <lofiboyog@gmail.com>
2025-11-04 20:37:37 +0000

    add a page for tools

--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -1,3 +1,4 @@
+
 import AppLayout from '@/components/app-layout';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
