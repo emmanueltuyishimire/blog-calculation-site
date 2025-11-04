41b65118ba7bf49f30fe07049c68ce9716113685
lofi boy <lofiboyog@gmail.com>
2025-11-04 20:16:43 +0000

    please retrive from github deployment mode to firebase studio developmen

--- next.config.ts
+++ next.config.ts
@@ -7,11 +7,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: './',
 };
 
 export default nextConfig;
