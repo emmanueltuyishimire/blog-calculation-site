366df53d1d4e6fc525db2d4b0f2add73001bdb27
lofi boy <lofiboyog@gmail.com>
2025-11-05 22:08:11 +0000

    duplicate header please

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -1,13 +1,8 @@
 
 'use client';
 
-import AppHeader from './app-header';
-
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <AppHeader />
       <main className="flex-1">{children}</main>
-    </>
   );
 }
